e5cf333707bf82433cbd724183aa561f454d0155
Jos Kuijpers <jkuijpers@jarvix.nl>
2019-02-22 22:58:52 +0100

    Initial Commit

---
+++ ReactionDiffusion/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ReactionDiffusion
+//
+//  Created by Jos Kuijpers on 22/02/2019.
+//  Copyright © 2019 Jos Kuijpers. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ReactionDiffusion/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ReactionDiffusion
+//
+//  Created by Jos Kuijpers on 22/02/2019.
+//  Copyright © 2019 Jos Kuijpers. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
